b6de358e7024217fa548b813693396ea7cb877a7
Jonas Burger <post@jonas-burger.de>
2021-05-23 18:17:33 +0200

    fixed even more bugs (a04c works now)

--- a04/shader/shaderC.fs.glsl
+++ a04/shader/shaderC.fs.glsl
@@ -28,12 +28,11 @@ vec2 reflection(vec3 facet_normal_view_space)
     vec2 lookup_coord = facet_normal_view_space.xy;
     // TODO: Calculate reflection vector in _view_ space
     vec3 camera_location_vector = normalize(-position_view_space.xyz);
-    vec3 reflection_vector_view_space = 2*dot(camera_location_vector, normal_view_space)*normal_view_space - camera_location_vector;
-
+    //vec3 reflection_vector_view_space = 2*dot(camera_location_vector, normal_view_space)*normal_view_space - camera_location_vector;
+    vec3 reflection_vector_view_space = reflect(-camera_location_vector, facet_normal_view_space);
+    
     // TODO: Transform reflection vector into _world_ space
     vec3 reflection_vector_world_space = view_to_world * reflection_vector_view_space;
-
-
 
 
     // TODO: transform reflection vector into polar texture coordinates (see shperical_coordinates.inc.glsl)
@@ -60,42 +59,16 @@ void main()
     for(int i = 0; i < glossyRays; ++i){
         vec3 n_tangent_space = vec3(1,0,0);
         vec2 u = vec2(random_uniform(N.xy, i*2), random_uniform(N.xy, i*2+1)); // parameters are kinda random
-        float z0 = sqrt(-2*log(u.x))*cos(2*PI*u.y);
-        float z1 = sqrt(-2*log(u.x))*sin(2*PI*u.y);
+        float z0 = sqrt(-2.f*log(u.x))*cos(2.f*PI*u.y);
+        float z1 = sqrt(-2.f*log(u.x))*sin(2.f*PI*u.y);
         n_tangent_space.y = z0*roughness;
         n_tangent_space.z = z1*roughness;
         vec3 n_view_space = normalize(n_tangent_space.x * N + n_tangent_space.y * T + n_tangent_space.z * B);
 
         vec2 lookup_coord = reflection(n_view_space);
-        refl_color += (debugUV ? vec3(lookup_coord,0) : texture(envMap, lookup_coord, -30).rgb) / glossyRays;
+        refl_color += debugUV ? vec3(lookup_coord,0) : texture(envMap, lookup_coord, -30).rgb;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    refl_color /= glossyRays;
 
 
 

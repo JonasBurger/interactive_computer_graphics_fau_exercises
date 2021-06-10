4a14b576ca6242048a820df0ccc756e17519bc35
Jonas Burger <post@jonas-burger.de>
2021-06-10 18:35:53 +0200

    b) works a bit

--- a06/shader/tes_eval.glsl
+++ a06/shader/tes_eval.glsl
@@ -14,8 +14,13 @@ uniform mat4 projection;
 void main()
 {
     // TODO: a) interpolate position
-    vec3 posBottom = mix(gl_in[0].gl_Position.xyz, gl_in[1].gl_Position.xyz, gl_TessCoord.x);
-    vec3 posTop = mix(gl_in[3].gl_Position.xyz, gl_in[2].gl_Position.xyz, gl_TessCoord.x);
+    vec3 posBL = gl_in[0].gl_Position.xyz;
+    vec3 posBR = gl_in[1].gl_Position.xyz;
+    vec3 posTL = gl_in[3].gl_Position.xyz;
+    vec3 posTR = gl_in[2].gl_Position.xyz;
+
+    vec3 posBottom = mix(posBL, posBR, gl_TessCoord.x);
+    vec3 posTop = mix(posTL, posTR, gl_TessCoord.x);
     vec3 pos = mix(posBottom, posTop, gl_TessCoord.y);
 
 
@@ -23,6 +28,8 @@ void main()
 
 
     // TODO: b) use heightmap to adjust height
+    pos.y = texture(heightmap, pos.xz).r * height_scale;
+
 
 
 
@@ -31,7 +38,31 @@ void main()
     gl_Position = projection * view * vec4(position, 1.0);
 
     // TODO: b) calculate the normal of the vertex
-    normal = vec3(0, 1, 0);
+
+    // adjust pos* from highmap too
+    posBL.y = texture(heightmap, posBL.xz).r * height_scale;
+    posBR.y = texture(heightmap, posBR.xz).r * height_scale;
+    posTL.y = texture(heightmap, posTL.xz).r * height_scale;
+    posTR.y = texture(heightmap, posTR.xz).r * height_scale;
+
+    vec3 posTobl = posBL - pos;
+    vec3 posTobr = posBR - pos;
+    vec3 posTotl = posTL - pos;
+    vec3 posTotr = posTR - pos;
+    //if(posTobl == vec3(0.f)) posTobl=vec3(-1, 0, -1);
+    //if(posTobr == vec3(0.f)) posTobr=vec3(1, 0, -1);
+    //if(posTotl == vec3(0.f)) posTotl=vec3(-1, 0, 1);
+    //if(posTotr == vec3(0.f)) posTotr=vec3(1, 0, 1);
+    
+    vec3 normalLeft = cross(posTobl, posTobr);
+    vec3 normalRight = cross(posTotr, posTotl);
+    normal = -normalize(normalize(normalLeft) + normalize(normalRight));
+
+    // TODO: fix normal calculation
+
+    normal = vec3(0, 1, 0); 
+    //U = 
+    //normal = 
 
 
 
